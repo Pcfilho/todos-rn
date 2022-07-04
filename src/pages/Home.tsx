f9191a74b848fc763caba55bed2e4ece35f57e25
Paulo César <paulofilhobarroso@gmail.com>
2022-07-04 14:34:11 -0300

    Concluding challenge

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,28 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const data : Task = {
+        id: (new Date().getTime() + Math.random() * 1000000).toFixed(0),
+        title: newTaskTitle,
+        done: false
+    }
+    setTasks(oldState => [...oldState, data]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => ({ ...task }))
+    updatedTasks.find((task) => {
+      if (task.id === id) {
+        task.done = !(task.done);
+      }
+    })
+
+    setTasks(updatedTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const updatedTasks = tasks.filter(task => task.id !== id);
+    setTasks(updatedTasks);
   }
 
   return (
